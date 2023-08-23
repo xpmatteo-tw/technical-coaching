b4d74ecfe097e9ed364a2b722e1194a66811789c
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-08-23 11:29:00 +0200

    I prefer test( to it(

--- greeter_test.mjs
+++ greeter_test.mjs
@@ -3,7 +3,7 @@
 import { Greeter } from './greeter.mjs';
 
 describe('greeter', () => {
-  it('works?', () => {
+  test('test runner is set up correctly', () => {
     expect(0).toEqual(1);
   });
 });
