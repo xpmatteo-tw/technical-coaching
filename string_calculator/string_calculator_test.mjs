f712bed1cfb3ea441748b5878c8b64a2ef0bed3c
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-08-29 09:40:39 +0200

    Started new directories for every kata

---
+++ string_calculator/string_calculator_test.mjs
@@ -0,0 +1,7 @@
+'use strict';
+
+import StringCalculator from './string_calculator.mjs';
+
+test('test runner is set up correctly', () => {
+    expect(0).toEqual(1);
+});
---
+++ string_calculator/test.sh
@@ -0,0 +1,2 @@
+export NODE_OPTIONS="--experimental-vm-modules --redirect-warnings=/tmp/node-warnings.log"
+jest $*
