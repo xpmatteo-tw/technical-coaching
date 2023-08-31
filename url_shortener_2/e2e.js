4851625429312fb399930f35f6ff7d056908186f
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-08-31 10:07:58 +0200

    url_shortener_2: add e2e test for hello

--- url_shortener_2/e2e.js
+++ url_shortener_2/e2e.js
@@ -2,11 +2,11 @@
 
 async function postJSON(url, data) {
     const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
     });
 
     expect(response.status).toBe(201);
@@ -19,19 +19,24 @@ async function getJSON(url) {
     return response.json();
 }
 
-const base_url='http://localhost:8080'
+const base_url = 'http://localhost:8080'
+
+test('hello, world', async () => {
+    const response = await getJSON(`${base_url}/hello`);
+    expect(response.message).toEqual("Hello, world!");
+});
 
 
 test('shortens the url and retrieves it', async () => {
-  const payload = {url: "https://www.google.com"};
-  const post_url = `${base_url}/shorten`;
-  const response = await postJSON(post_url, payload);
-  const shortened_url = response.shortened_url;
-  expect(shortened_url).toMatch(/https:\/\/tw.ks\/\d+/);
+    const payload = {url: "https://www.google.com"};
+    const post_url = `${base_url}/shorten`;
+    const response = await postJSON(post_url, payload);
+    const shortened_url = response.shortened_url;
+    expect(shortened_url).toMatch(/https:\/\/tw.ks\/\d+/);
 
-  const get_url = `${base_url}?shortened_url=${shortened_url}`;
-  const response2 = await getJSON(get_url);
-  expect(response2.original_url).toEqual("https://www.google.com");
+    const get_url = `${base_url}?shortened_url=${shortened_url}`;
+    const response2 = await getJSON(get_url);
+    expect(response2.original_url).toEqual("https://www.google.com");
 });
 
 
