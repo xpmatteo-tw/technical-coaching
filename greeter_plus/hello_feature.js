e0c82e81729e8abd633d9c28766505447188384c
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-09-01 11:15:54 +0200

    Rename url_shortener_2 to greeter_plus

---
+++ greeter_plus/hello_feature.js
@@ -0,0 +1,4 @@
+function serve_greet(searchParams) {
+}
+
+module.exports = serve_greet;
---
+++ greeter_plus/hello_feature_test.js
@@ -0,0 +1,5 @@
+const greet_service = require('./hello_feature.js');
+
+test('tests work', () => {
+    expect(0).toBe(1);
+});
